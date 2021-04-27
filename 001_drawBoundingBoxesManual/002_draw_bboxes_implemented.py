c4592980b0fb8f73fa7810d49575fac3922e0395
remichartier <remipr.chartier@gmail.com>
2021-04-27 00:59:06 -0700

    implemented

--- 001_drawBoundingBoxesManual/002_draw_bboxes_implemented.py
+++ 001_drawBoundingBoxesManual/002_draw_bboxes_implemented.py
@@ -13,10 +13,14 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # make a copy of the image
     draw_img = np.copy(img)
     # draw each bounding box on your image copy using cv2.rectangle()
+    for box in bboxes :
+        #print(box[0])
+        cv2.rectangle(draw_img, box[0], box[1], color, thick)
     # return the image copy with boxes drawn
     return draw_img # Change this line to return image copy with boxes
 # Add bounding boxes in this format, these are just example coordinates.
-bboxes = [((100, 100), (200, 200)), ((300, 300), (400, 400))]
+bboxes = [((275, 480), (375, 570)), ((470, 500), (545, 560)),((580, 500), (640, 550)),
+          ((830, 500), (1125, 675))]
 
 result = draw_boxes(image, bboxes)
 plt.imshow(result)
